433db904e225ecc9cc65c1726faed3679209b735
Ruthvik <ruthvik.2123@gmail.com>
2025-03-21 23:50:02 +0530

    added spike obstacle

---
+++ circleoflife/enemies/spikepivot.gd
@@ -0,0 +1,15 @@
+extends Marker2D
+
+@export var rotation_speed = -2
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	rotation += rotation_speed * delta 
+
+
+func _on_spike_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		area.queue_free()
+		rotation_speed *= -1
